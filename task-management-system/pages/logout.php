fefbac267ba0d8d54e5e8ccd97c7a02ca97e10e2
johnbedeir <john.bedeir@gmail.com>
2024-01-13 19:06:18 +0100

    create-logout-page

--- task-management-system/pages/logout.php
+++ task-management-system/pages/logout.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
-session_destroy();
-header("Location: login.php");
-exit();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logout</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    
+    <header class="app-header">
+        <a href="../index.php" class="header-link">Task Management System</a>
+    </header>
+
+    <div>
+        <h2>Thank you</h2>
+    </div>
+
+    <script src="../js/script.js"></script>
+</body>
+</html>
