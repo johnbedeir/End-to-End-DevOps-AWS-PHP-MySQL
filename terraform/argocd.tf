e9d0489e2456ce331d536c0557f326023a5c8598
johnbedeir <john.bedeir@gmail.com>
2024-01-28 23:57:59 +0100

    Fix #15

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -12,12 +12,12 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.53.4" # Specify the version of the Argo CD chart you want to use
+  version          = "5.53.4"
   cleanup_on_fail  = true
   namespace        = "argocd"
   create_namespace = true
 
   values = [var.argocd_values]
 
-  depends_on = [module.eks.cluster_id] # Replace with your EKS module or remove if not needed
+  depends_on = [module.eks.cluster_id]
 }
